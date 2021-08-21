a93c64d7faed29a0843fbef8bafe7ae4e0709f2f
aijaz90 <aijazmike90@gmail.com>
2021-08-21 23:48:09 +0500

    Initially Completed Country State and City picker

---
+++ CountryStateCityPicker/Constants/StringConstants.swift
@@ -0,0 +1,19 @@
+//
+//  StringConstants.swift
+//  CountryStateCityPicker
+//
+//  Created by Aijaz Ali on 8/21/21.
+//
+
+import Foundation
+
+extension String {
+    static let select = { return "Select " }()
+    static let countryPicker = { return "Country" }()
+    static let statePicker = { return "State" }()
+    static let cityPicker = { return "City" }()
+    static let json = { return "json" }()
+    static let countryJsonName = { return "synergistic_golfcountries" }()
+    static let stateJsonName = { return "synergistic_golf.states" }()
+    static let cityJsonName = { return "synergistic_golfcities" }()
+}
